e81c1ee11a2cc6715db77413f7e6cf82b598d55b
Promode <pramoddutta_96@yahoo.com>
2024-09-23 15:42:40 +0530

    All the files - 23 sept

--- src/sept/ex_23092024/Lab090.java
+++ src/sept/ex_23092024/Lab090.java
@@ -2,14 +2,18 @@ package sept.ex_23092024;
 
 public class Lab090 {
     public static void main(String[] args) {
-        char c = 'A';
+        char c = 'C';
         switch (c) {
+            case 'A'+1:
+                System.out.println("B");
+                break;
             case 'A':
                 System.out.println("A");
                 break;
-//            case 65:
-//                System.out.println("65");
-//                break;
+            case 'A'+2:
+                System.out.println("C");
+                break;
+
         }
     }
 }
